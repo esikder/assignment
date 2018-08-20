12076428a60f92a967905f7b52c0ab882515d2b5
Erfana Sikder <erfanasikder@nbnco.com.au>
2018-08-20 14:08:24 +1000

    ff

--- UiTest/src/test/java/helper/BrowserLibrary.java
+++ UiTest/src/test/java/helper/BrowserLibrary.java
@@ -2,15 +2,34 @@ package helper;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.IOException;
 
 public class BrowserLibrary {
-
     public WebDriver getbrowser() throws IOException {
+        WebDriver driver;
 
-        System.setProperty("webdriver.chrome.driver", ConfigHandler.getConfigValue("chromePath"));
-        WebDriver driver = new ChromeDriver();
+        switch(System.getenv("BROWSER")){
+
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", ConfigHandler.getConfigValue("chromePath"));
+                driver = new ChromeDriver();
+                break;
+            case "ff":
+                System.setProperty("webdriver.gecko.driver", ConfigHandler.getConfigValue("ffPath"));
+                driver = new FirefoxDriver();
+                break;
+            default:
+                System.setProperty("webdriver.chrome.driver", ConfigHandler.getConfigValue("chromePath"));
+                driver = new ChromeDriver();
+                break;
+
+
+        }
+
+
+
 
         return driver;
     }
