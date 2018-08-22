ef16125ea4eb47d282add6794c7a768d8bc79d7f
Erfana Sikder <erfanasikder@nbnco.com.au>
2018-08-22 14:13:09 +1000

    restructured

--- UiTest/src/test/java/helper/ConfigHandler.java
+++ UiTest/src/test/java/helper/ConfigHandler.java
@@ -1,23 +1,25 @@
 package helper;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigHandler {
 
     public static String getConfigValue(String key)  {
-        InputStream inputStream;
-        Properties prop = new Properties();
-        String propFileName = "config.properties";
-        inputStream = ConfigHandler.class.getClassLoader().getResourceAsStream(propFileName);
+        Properties prop =  new Properties();
+        InputStream input = null;
         try {
-        if (inputStream != null)
-            prop.load(inputStream);
+            input = new FileInputStream("config.properties");
+            prop.load(input);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (Exception e){
-            System.out.println(e.getStackTrace());
 
-        }
         return  prop.getProperty(key);
         }
 
